25a590343b2a3866ecdaade509c4c529bbc1753c
Aditya Singh <aditya@cleverstudio.in>
2023-04-04 19:40:56 +0530

    Update WebSecurityConfiguration.java

--- src/main/java/com/dropandgo/backend/config/WebSecurityConfiguration.java
+++ src/main/java/com/dropandgo/backend/config/WebSecurityConfiguration.java
@@ -14,7 +14,8 @@ public class WebSecurityConfiguration implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedOrigins(
                         "http://192.168.1.107:3000",
-                        "http://localhost:3000"
+                        "http://localhost:3000",
+            "http://127.0.0.1:5173/"
                 )
                 .allowedHeaders("*")
                 .exposedHeaders(
